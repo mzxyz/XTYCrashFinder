ad41c1057ad42236d4e1cf3094d82955adf8235e
Sunday <Sunday>
2016-08-04 18:09:21 +0800

    add annotation

--- CrashFinder/XTYCrashFinder.h
+++ CrashFinder/XTYCrashFinder.h
@@ -10,6 +10,9 @@
 
 @interface XTYCrashFinder : NSObject
 
+/**
+ *  install the XTYCrashFinder, you should use this method at the end of the appdidLanch method. This is because some other third party library may also collect crash information.
+ */
 + (BOOL)installNSExceptionHandler;
 + (void)unInstallNSExceptionHandler;
 
